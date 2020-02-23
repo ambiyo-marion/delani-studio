b32f23b44af3ce7a3ece228277fa5c616647e39c
ambiyo-marion <ambiyomarion@gmail.com>
2020-02-23 18:51:23 +0300

    added the toggle effect on design development and management

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#icondesign").click(function() {
+      $("p").toggle();
+    });
+    $("#development").click(function() {
+        $("p").toggle();
+      });
+      $("#management").click(function() {
+        $("p").toggle();
+      });
+});
