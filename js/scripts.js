daf57dc5b601b07465a3d8f2f869c4f7265b7a76
ambiyo-marion <ambiyomarion@gmail.com>
2020-02-24 09:23:26 +0300

    added hoover effect

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,39 @@ $(document).ready(function() {
       $("#management").click(function() {
         $(".managementdescription").toggle();
       });
+      $(".work4-image").hover(function(){
+        $("#work4-blue").toggle()
+        $("#work4-img").toggle()
+    });
+    $(".work3-image").hover(function(){
+      $("#work3-ontario").toggle()
+      $("#work3-img").toggle()
+  });
+  $(".work2-image").hover(function(){
+      $("#work2-gold").toggle()
+      $("#work2-img").toggle()
+  });
+  $(".work8-image").hover(function(){
+      $("#work8-giraffe").toggle()
+      $("#work8-img").toggle()
+  });
+  $(".work1-image").hover(function(){
+      $("#work1-entertain").toggle()
+      $("#work1-img").toggle()
+  });
+  $(".work7-image").hover(function(){
+      $("#work7-log").toggle()
+      $("#work7-img").toggle()
+  });
+  $(".work6-image").hover(function(){
+      $("#work6-form").toggle()
+      $("#work6-img").toggle()
+  });
+  $(".work5-image").hover(function(){
+      $("#work5-elena").toggle()
+      $("#work5-img").toggle()
+  });
+      
       $("#submitbutton").click(function(){
         var messageHere = (document.getElementById("messagehere").value);
         var name = (document.getElementById("name").value);
